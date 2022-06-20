bbd9e773e45c095a4afeebdaed36822acc12b2fd
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-20 15:55:37 -0700

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_20_215926) do
+ActiveRecord::Schema.define(version: 2022_06_20_225340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "quotes", force: :cascade do |t|
+    t.string "content"
+    t.string "author"
   end
 
 end
