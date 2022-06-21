2779d8c4d0e3ed92fbbe534b209737fc5130a3d1
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-20 23:13:03 -0700

    add scope for search

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,6 @@
 class Quote < ApplicationRecord
   validates :author, presence: true
   validates :content, presence: true
+
+  scope :search, -> (name_parameter) { where("author like ?", "%#{name_parameter}%")}
 end
