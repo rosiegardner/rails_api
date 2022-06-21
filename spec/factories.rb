d97aca175ca94de3af285f0b698652f6de27b4f2
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-20 20:57:14 -0700

    add date to factorybot method

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,5 +2,8 @@ FactoryBot.define do
   factory(:quote) do
     author {Faker::Name.name}
     content {Faker::Movie.quote}
+    date {"today"}
   end
 end
+
+# let!(:quotes) { FactoryBot.create_list(:quote, 20)}
