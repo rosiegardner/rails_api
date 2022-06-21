b01ccefc90ab1ee323c10097774c879450dd6390
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-20 21:59:40 -0700

    add testing - passes tests

---
+++ spec/requests/exceptions_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+
+describe 'get a quote route with exception handler', :type => :request do
+
+  before { get '/quotes/200' }
+
+  it 'returns a RecordNotFound error' do
+    expect(response).to have_http_status(:not_found)
+  end
+end
+
+describe 'post a quote route with exception handlers', :type => :request do
+
+  before do
+    post '/quotes', params: { :author => "test_author", :content => nil}
+  end
+
+  it 'returns a RecordInvalid error' do
+    expect(response).to have_http_status(422)
+  end
+end
