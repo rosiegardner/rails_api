d6f2ad0b84abcaabbd88fc56f099ef60be3cadb7
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-20 23:16:17 -0700

    change scope for case sensitivity

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,5 +2,5 @@ class Quote < ApplicationRecord
   validates :author, presence: true
   validates :content, presence: true
 
-  scope :search, -> (name_parameter) { where("author like ?", "%#{name_parameter}%")}
+  scope :search, -> (name_parameter) { where("author ilike ?", "%#{name_parameter}%")}
 end
